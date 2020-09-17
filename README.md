36af60d97f5abdf65157dcd016de96c1911942db
Thomas Cannon <nerdstoke@gmail.com>
2020-09-17 18:31:51 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # raspi-gpio-ext
 Modules that control different hardware pieces from GPIOs, abstracting out some lower level stuff
+
+## Everything is highly developmental right now. Little if anything will be working
+
+## Installation
+
+#### Production Use:
+
+initial install
+`pip install git+https://github.com/nerdstoke/raspi-gpio-ext.git#egg=measurements`
+update to latest version
+`pip install --upgrade git+https://github.com/nerdstoke/raspi-gpio-ext.git#egg=measurements`
+
+#### Development:
+
+`git clone https://github.com/gamma-ray-burst/raspi-gpio-ext.git`  
+`cd raspi-gpio-ext`  
+`python setup.py develop`  
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+# read the contents of markdown README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='raspi-gpio-ext',
+    url='https://github.com/nerdstoke/raspi-gpio-ext',
+    author='Thomas Cannon',
+    author_email='nerdstoke@gmail.com',
+    packages=['raspi-gpio-ext'],
+    install_requires=['gpiozero>=1.5.1'],
+    version='0.1',
+    license='MIT',
+    description='An example of a python package from pre-existing code',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+)
